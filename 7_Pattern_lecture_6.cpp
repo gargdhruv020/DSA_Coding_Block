e7cc0d25ba9bbf60634edf697c54437e237bf234
gargdhruv020 <gargdhruv020@gmail.com>
2026-02-18 10:24:27 +0530

    notes

--- 7_Pattern_lecture_6.cpp
+++ 7_Pattern_lecture_6.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=n;i>=1;i--){
+    for(int i=1;i<=n;i++){
         //spaces
         for(int j=1;j<=n-i;j++){
             cout<<"  ";
         }
         //stars
-        for(int j=1;j<=i;j++){
+        for(int j=1;j<=2*i-1;j++){
             cout<<"* ";
         }
         cout<<endl;
